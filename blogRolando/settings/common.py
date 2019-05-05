7885ffef8fbb86fce047bed06ab947acc1d5ae07
yeco <oscco.yoshin@gmail.com>
2019-05-05 09:55:49 -0500

    adding templates models

--- blogRolando/settings/common.py
+++ blogRolando/settings/common.py
@@ -135,3 +135,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR + '/static'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR + '/media'
--- blogRolando/urls.py
+++ blogRolando/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	re_path('',TemplateView.as_view(template_name='index.html'), name='home'),
+	path('',TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('curso/',include('cursos.urls'))
 ]
